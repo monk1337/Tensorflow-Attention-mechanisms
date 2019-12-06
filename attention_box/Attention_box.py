dbd52a5a63939db66778f61551f7c42f3adb6b0a
monk1337 <3x3paul@gmail.com>
2019-12-06 19:51:14 +0530

    check

--- attention_box/Attention_box.py
+++ attention_box/Attention_box.py
@@ -3,32 +3,40 @@ import tensorflow as tf
 class Attention_box(object):
 
     @staticmethod
-    def soft_attention(query, attention_dim, visualize_attention = False):
+    def soft_attention(query, visualize_attention = False):
 
         # query is three dim tensor
         # batch x max sequence length x dim { if output is from bi-lstm }
         # other output also should be three dim
+        # example if logit is   12 x 10 x 24
 
+
+        # dim_shape = 24
         dim_shape      = query.shape[2]
         reshape_tensor = tf.reshape(query,[-1,dim_shape])
+        # reshape : 12 x 10 x 24 ==>  120 x 24
 
-
+        # 24 x 1
         attention_size = tf.get_variable(name='attention_size',
-                                         shape=[dim_shape,attention_size],
+                                         shape=[dim_shape,1],
                                          dtype=tf.float32,
                                          initializer=tf.random_uniform_initializer(-0.01,0.01))
         # bias 1
-        bias          = tf.get_variable(name='bias',shape=[attention_size],
+        bias          = tf.get_variable(name='bias',shape=[1],
                                         dtype=tf.float32,
                                         initializer=tf.random_uniform_initializer(-0.01,0.01))
 
 
+        #projection without activation 
+        #ex : 120x24 matmul 24x 1 ==> 120x1
         attention_projection = tf.add(tf.matmul(reshape_tensor,attention_size),bias)
+        #reshape . 120x1 ==> 12x10x1 (shape of input )
         output_reshape       = tf.reshape(attention_projection,[tf.shape(query)[0],tf.shape(query)[1],-1])
+        #softmax over logits 12x10x1
         attention_output     = tf.nn.softmax(output_reshape,dim = 1)
 
 
-
+        # reshape for 2-D for visualization
         attention_visualize = tf.reshape(attention_output,
                                          [tf.shape(query)[0],
                                           tf.shape(query)[1]],
@@ -48,6 +56,18 @@ class Attention_box(object):
                     'visualize vector': attention_visualize,
                     'reduced_output'   : Final_output
                     }
+
+        else:
+
+            return {
+                    'attention_output': attention_projection_output,
+                    'reduced_output'   : Final_output
+                    }
+
+
+    @staticmethod
+    def hard_attention():
+        
 
                 
         
